166b413866741a46acc8327f0ea1bc4c8d2cefdc
Chiranjith18 <145703565+Chiranjith18@users.noreply.github.com>
2024-12-07 23:50:20 +0530

    Update Latte.java

--- Latte.java
+++ Latte.java
@@ -1,16 +1,16 @@
 import com.sun.jdi.InvalidTypeException;
 
 public class Latte extends Coffee {
+
     String milkType;
     String syrupFlavor;
 
-    // TODO 12: declare that the constructors throws a InvalidTypeException
+    // Declare that the constructor throws an InvalidTypeException
     public Latte(String name, String roast, double price, String milkType, String syrupFlavor) throws IllegalArgumentException {
         super(name, roast, price);
 
-        // TODO 11: check whether a valid milkType is selected, if not throw an exception with a message
-        if (!("whole".equals(milkType)) && !("skim".equals(milkType)) && !("almond".equals(milkType)) && !("oat".equals(milkType))) {
-            throw new IllegalArgumentException("please select a valid milk type!");
+        if (!"whole".equals(milkType) && !"skim".equals(milkType) && !"almond".equals(milkType) && !"oat".equals(milkType)) {
+            throw new IllegalArgumentException("Please select a valid milk type!");
         }
 
         this.milkType = milkType;
@@ -19,6 +19,6 @@ public class Latte extends Coffee {
 
     public void printLatteDetails() {
         System.out.println("Your latte has " + milkType + " milk and " + syrupFlavor + " flavor.");
-        System.out.println("Your total bill is " + price + "$.");
+        System.out.println("Your total bill is $" + price + ".");
     }
 }
